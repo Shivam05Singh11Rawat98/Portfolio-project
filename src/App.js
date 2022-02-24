e99f3f3f7b87aa4e7deeeb4216a8cf87c6654159
Saujan ya Tiwari <saujanyatiwari455@gmail.com>
2022-02-24 20:12:21 +0530

    thank me later

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'jquery';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar } from './components/Navbar';
 import { Header } from './components/Header';
-import Particles from "react-tsparticles";
 import About from './components/About';
 import Projects from './components/Projects';
 import Education from './components/Education';
@@ -14,46 +13,6 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
-      <Particles
-        id="tsparticles"
-      canvasClassName='class-particle'
-        options={{
-          fullScreen: { enable: false, zIndex: 0 },
-          fpsLimit: 60,
-          particles: {
-            color: {
-              value: "#fa9b00",
-            },
-            size:{
-              value:3
-            },
-            opacity:{
-              value:0.30
-            },
-            links: {
-              enable: true,
-              color: "#fa9b00",
-              distance: 150,
-              opacity: 0.30
-            },
-            move: {
-            direction: "none",
-            enable: true,
-            outMode: "bounce",
-            random: false,
-            speed: 2,
-            straight: false,
-          },
-            number: {
-            density: {
-              enable: true,
-              area: 800,
-            },
-            value: 80,
-          },
-          },
-          detectRetina: true,
-        }} />
       <Navbar />
       <Header />
       <About/>
